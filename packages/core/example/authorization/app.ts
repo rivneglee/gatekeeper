7aca7f12164467fa35c89e236f20a4ee303f8fa4
Cheng Li <cheng.li@myob.com>
2020-03-17 11:33:25 +1100

    Update example

--- packages/core/example/authorization/app.ts
+++ packages/core/example/authorization/app.ts
@@ -11,6 +11,8 @@ const gateKeeper = createGatekeeperMiddleware(
   () => ({ token: { userId: 'foo' } }),
 );
 
+app.use(gateKeeper);
+
 const orders: any = {
   123: { userId: 'others', amount: 123.00 },
   321: { userId: 'foo', amount: 100.00 },
@@ -21,11 +23,11 @@ const users: any[] = [
   { userId: 'gateKeeper', name: 'Admin' },
 ];
 
-app.get('/:userId/orders/:orderId', gateKeeper, (request: Request, response: Response) => {
+app.get('/:userId/orders/:orderId', (request: Request, response: Response) => {
   response.json(orders[request.params.orderId]);
 });
 
-app.get('/admin/users', gateKeeper, (request: Request, response: Response) => {
+app.get('/admin/users', (request: Request, response: Response) => {
   response.json(users);
 });
 
